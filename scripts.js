b72af826af6d264dac201edb42ab5f757bb8be07
Marcin Wojdat <wojdatm98@gmail.com>
2020-12-02 11:14:57 +0100

    Adds displaying repos on page

--- scripts.js
+++ scripts.js
@@ -7,7 +7,28 @@ const fetchReposByUserName = async (user_name) => {
 function findRepos() {
     event.preventDefault();
     const user_name = document.getElementById("user_name").value;
+    const repos = document.getElementById("repos")
+    
+    
     fetchReposByUserName(user_name).then(response => {
-        console.log(response);
+        repos.innerHTML = "";
+
+        for (let i = 0; i < response.length; ++i) {
+            const repo = response[i];
+            const info = [repo.name, repo.owner.login, repo.stargazers_count, repo.language];
+            
+            let cln = document.getElementsByClassName("repo_info")[0].cloneNode(true);
+            cln.style.display = "block";
+            
+            for (let i = 0; i < info.length; i++) {
+                cln.children[i].children[0].innerHTML = info[i];
+            }
+          
+            try {
+                repos.appendChild(cln);
+            } catch(e) {
+                consol.error(e);
+            }
+        }
     });
 }
