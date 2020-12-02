67384c0cb5ef48ad2d645830ce5e405e9fcfa3b2
Marcin Wojdat <wojdatm98@gmail.com>
2020-12-02 14:30:53 +0100

    Adds comments in js file

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+/* Searches for specific user's repositories 
+ * Arguments:
+ *  - user_name:string - name of the user 
+ * Returns:
+ *  - array of user's repositories in JSON format 
+ *  - empty array if user does not exist */
 const fetchReposByUserName = async (user_name) => {
     const api_call = await fetch(`https://api.github.com/search/repositories?q=user:${user_name}`);
     if (api_call.status != 200) {
@@ -9,6 +15,13 @@ const fetchReposByUserName = async (user_name) => {
     }
 };
 
+/* Searches for specific user's repositories with specific name
+ * Arguments:
+ *  - user_name:string - name of the user
+ *  - repo_name:string - name of the repository 
+ * Returns:
+ *  - array of user's repositories with specific name in JSON format 
+ *  - empty array if no results found */
 const fetchReposByUserNameAndRepoName = async (user_name, repo_name) => {
     const api_call = await fetch(`https://api.github.com/search/repositories?q=user:${user_name}+${repo_name}+in:name`);
     if (api_call.status != 200) {
@@ -20,6 +33,12 @@ const fetchReposByUserNameAndRepoName = async (user_name, repo_name) => {
     }
 };
 
+/* Searches for repositories with specific name
+ * Arguments:
+ *  - repo_name:string - name of the repository 
+ * Returns:
+ *  - array of repositories with specific name in JSON format 
+ *  - empty array if no results found */
 const fetchReposByRepoName = async (repo_name) => {
     const api_call = await fetch(`https://api.github.com/search/repositories?q=${repo_name}+in:name`);
     if (api_call.status != 200) {
@@ -31,11 +50,17 @@ const fetchReposByRepoName = async (repo_name) => {
     }
 };
 
+/* Shows found results on the website or shows error message if gets empty array
+ * Arguments:
+ *  - repos:Array - array of repositories in JSON format 
+ * Returns:
+ *  - nothing */
 function displayRepos(repos) {
     const error_message_containter = document.getElementById("error_message");
     const repos_container = document.getElementById("repos");
     repos_container.innerHTML = "";
     
+    // Displaying hiding error messsage
     if (repos.length == 0) {
         error_message.innerHTML = "Brak pasujących wyników";
     }
@@ -47,14 +72,17 @@ function displayRepos(repos) {
         const repo = repos[i];
         const info = [repo.name, repo.owner.login, repo.stargazers_count, repo.language];
         
+        // Cloning repo_info template
         const cln = document.getElementsByClassName("repo_info")[0].cloneNode(true);
         cln.style.display = "block";
         cln.href = repo.html_url;
         
+        // Updating values of attributes
         for (let i = 0; i < info.length; i++) {
             cln.children[i].children[0].innerHTML = info[i];
         }
-      
+        
+        // Adding new div to #repos
         try {
             repos_container.appendChild(cln);
         } catch(e) {
@@ -63,6 +91,11 @@ function displayRepos(repos) {
     }
 }
 
+/* looks for repositories according to the given data (user's name, repo's name) in the #form
+ * Arguments:
+ *  - nothing
+ * Returns:
+ *  - nothing */
 function findRepos() {
     event.preventDefault();
     const user_name = document.getElementById("user_name").value;
